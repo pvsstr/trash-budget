c975c03fb19ee60306a2bfc698f90b4d0e73d4d1
pvsstr <aprfpivdan@gmail.com>
2026-08-04 13:17:28 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,17 @@
-const CACHE_NAME = 'trash-budget-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+const CACHE='trash-v3';
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    .then(()=>clients.claim())
   );
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      const cp=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,cp));
+      return r;
+    }).catch(()=>caches.match(e.request))
   );
 });
