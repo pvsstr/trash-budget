fccff7c2867e1dcdf9d616a135e7e086b5cc0246
pvsstr <aprfpivdan@gmail.com>
2026-08-05 18:31:04 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-const CACHE='trash-v4';
+const CACHE='trash-v5';
 self.addEventListener('install',e=>{self.skipWaiting()});
 self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-    .then(()=>clients.claim())
-  );
+ e.waitUntil(
+ caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+ .then(()=>clients.claim())
+ );
 });
 self.addEventListener('fetch',e=>{
-  e.respondWith(
-    fetch(e.request).then(r=>{
-      const cp=r.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,cp));
-      return r;
-    }).catch(()=>caches.match(e.request))
-  );
+ e.respondWith(
+ fetch(e.request).then(r=>{
+ const cp=r.clone();
+ caches.open(CACHE).then(c=>c.put(e.request,cp));
+ return r;
+ }).catch(()=>caches.match(e.request))
+ );
 });
